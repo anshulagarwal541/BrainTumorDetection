22cce90a0a8a2538856969e901e3a0898339db12
Rachit Agarwal <123626141+anshulagarwal541@users.noreply.github.com>
2025-05-15 21:31:52 +0530

    updated

--- Client/src/pages/User/UserChat.jsx
+++ Client/src/pages/User/UserChat.jsx
@@ -76,34 +76,41 @@ function UserChat() {
 
     return (
         <div className='bg-pink-100 min-h-[100vh] text-primary flex flex-col'>
-            <div className='border-b-2 border-b-primary p-4 text-center text-2xl font-extrabold'>
-                {admin && admin.name}
-            </div>
-
-            <div className="flex flex-col flex-grow overflow-y-auto px-4 py-4 space-y-3">
-                {user && messages && messages.map((msg) => (
-                    <div key={msg.id} className={`flex ${msg.sender.id === user.userInfo.user.id ? 'justify-end' : 'justify-start'}`}>
-                        <div className={`max-w-xs p-3 rounded-xl ${msg.sender.id != user.userInfo.user.id ? 'bg-primary text-pink-100' : 'bg-gray-200 text-gray-800'}`}>
-                            {msg.message}
-                        </div>
+            {admin ?
+                <>
+                    <div className='border-b-2 border-b-primary p-4 text-center text-2xl font-extrabold'>
+                        {admin.name}
                     </div>
-                ))}
-            </div>
+
+                    <div className="flex flex-col flex-grow overflow-y-auto px-4 py-4 space-y-3">
+                        {user && messages && messages.map((msg) => (
+                            <div key={msg.id} className={`flex ${msg.sender.id === user.userInfo.user.id ? 'justify-end' : 'justify-start'}`}>
+                                <div className={`max-w-xs p-3 rounded-xl ${msg.sender.id != user.userInfo.user.id ? 'bg-primary text-pink-100' : 'bg-gray-200 text-gray-800'}`}>
+                                    {msg.message}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
 
 
-            <div className='p-4 border-t-2 border-t-primary'>
-                <form onSubmit={postMessage} className='flex items-center gap-2'>
-                    <input
-                        className='bg-primary px-5 py-2 text-pink-100 rounded-2xl flex-grow'
-                        placeholder='Enter your message here'
-                        type="text"
-                        name="message"
-                    />
-                    <button type="submit" className='bg-primary px-5 py-2 rounded-2xl text-bold text-pink-100 font-bold'>
-                        Send
-                    </button>
-                </form>
-            </div>
+                    <div className='p-4 border-t-2 border-t-primary'>
+                        <form onSubmit={postMessage} className='flex items-center gap-2'>
+                            <input
+                                className='bg-primary px-5 py-2 text-pink-100 rounded-2xl flex-grow'
+                                placeholder='Enter your message here'
+                                type="text"
+                                name="message"
+                            />
+                            <button type="submit" className='bg-primary px-5 py-2 rounded-2xl text-bold text-pink-100 font-bold'>
+                                Send
+                            </button>
+                        </form>
+                    </div>
+                </> :
+                <>
+                    <div className='text-primary font-extrabold text-center text-2xl'>No Doctor assigned yet...</div>
+                </>
+            }
         </div>
     );
 }
